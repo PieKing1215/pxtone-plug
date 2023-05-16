489f2611eaa1eed43cd13171b9de293f8c85ce52
David M <PieKing1215@users.noreply.github.com>
2023-05-16 01:24:37 -0400

    Set up rustfmt + run

--- pxtone-plug/src/lib.rs
+++ pxtone-plug/src/lib.rs
@@ -1,7 +1,10 @@
 use log::{LevelFilter, Log, RecordBuilder};
 use nih_plug::prelude::*;
-use pxtone_sys::{pxtnService, pxtnError_get_string, pxtnDescriptor, pxtnWoice, pxtnPulse_Frequency, pxtnVOICETONE};
-use simplelog::{WriteLogger, Config};
+use pxtone_sys::{
+    pxtnDescriptor, pxtnError_get_string, pxtnPulse_Frequency, pxtnService, pxtnVOICETONE,
+    pxtnWoice,
+};
+use simplelog::{Config, WriteLogger};
 use std::ffi::CStr;
 use std::fs::File;
 use std::sync::Arc;
@@ -52,7 +55,11 @@ impl Default for PtPlug {
         let logger = log_file.map(|f| WriteLogger::new(LevelFilter::Info, Config::default(), f));
 
         if let Some(logger) = &logger {
-            logger.log(&RecordBuilder::new().args(format_args!("LOG STARTED")).build());
+            logger.log(
+                &RecordBuilder::new()
+                    .args(format_args!("LOG STARTED"))
+                    .build(),
+            );
         }
 
         unsafe {
@@ -60,7 +67,11 @@ impl Default for PtPlug {
             match serv.init() {
                 0 => {
                     if let Some(logger) = &logger {
-                        logger.log(&RecordBuilder::new().args(format_args!("serv.init OK")).build());
+                        logger.log(
+                            &RecordBuilder::new()
+                                .args(format_args!("serv.init OK"))
+                                .build(),
+                        );
                     }
                 },
                 n => panic!(
@@ -74,11 +85,10 @@ impl Default for PtPlug {
                 panic!("freq Init");
             }
 
-
             let mut descriptor = pxtnDescriptor::new();
 
             let bytes = include_bytes!("../res/piano_EL4.ptvoice");
-    
+
             println!("Loading {} bytes", bytes.len());
             descriptor.set_memory_r(bytes as *const _ as *mut _, bytes.len() as i32);
 
@@ -88,7 +98,11 @@ impl Default for PtPlug {
             match woice.PTV_Read(&mut descriptor) {
                 0 => {
                     if let Some(logger) = &logger {
-                        logger.log(&RecordBuilder::new().args(format_args!("woice.PTV_Read OK")).build());
+                        logger.log(
+                            &RecordBuilder::new()
+                                .args(format_args!("woice.PTV_Read OK"))
+                                .build(),
+                        );
                     }
                 },
                 n => panic!(
@@ -100,7 +114,7 @@ impl Default for PtPlug {
             Self {
                 params: Arc::new(PtParams::default()),
                 sample_rate: 1.0,
-    
+
                 master_gain: Smoother::new(SmoothingStyle::Linear(5.0)),
 
                 pxtn_serv: serv,
@@ -123,17 +137,10 @@ impl Default for PtPlug {
 impl Default for PtParams {
     fn default() -> Self {
         Self {
-            gain: FloatParam::new(
-                "Gain",
-                -10.0,
-                FloatRange::Linear {
-                    min: -30.0,
-                    max: 0.0,
-                },
-            )
-            .with_smoother(SmoothingStyle::Linear(3.0))
-            .with_step_size(0.01)
-            .with_unit(" dB"),
+            gain: FloatParam::new("Gain", -10.0, FloatRange::Linear { min: -30.0, max: 0.0 })
+                .with_smoother(SmoothingStyle::Linear(3.0))
+                .with_step_size(0.01)
+                .with_unit(" dB"),
         }
     }
 }
@@ -152,15 +159,22 @@ impl PtPlug {
             for tone in &mut self.tones {
                 for v in 0..self.pxtn_woice._voice_num as usize {
                     let vi = &mut {
-                        std::slice::from_raw_parts_mut(self.pxtn_woice._voinsts, pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _)
+                        std::slice::from_raw_parts_mut(
+                            self.pxtn_woice._voinsts,
+                            pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _,
+                        )
                     }[v];
                     let vt = &mut tone.voice_tones[v];
-    
-                    unsafe fn update_env(vi: &mut pxtone_sys::pxtnVOICEINSTANCE, vt: &mut pxtnVOICETONE, on: bool) {
+
+                    unsafe fn update_env(
+                        vi: &mut pxtone_sys::pxtnVOICEINSTANCE,
+                        vt: &mut pxtnVOICETONE,
+                        on: bool,
+                    ) {
                         if vt.life_count <= 0 || vi.env_size == 0 {
                             return;
                         }
-    
+
                         if on {
                             if vt.env_pos < vi.env_size {
                                 vt.env_volume = *vi.p_env.offset(vt.env_pos as _) as _;
@@ -168,7 +182,8 @@ impl PtPlug {
                             }
                         } else {
                             if vt.env_pos < vi.env_release {
-                                vt.env_volume = vt.env_start + (-vt.env_start * vt.env_pos / vi.env_release);
+                                vt.env_volume =
+                                    vt.env_start + (-vt.env_start * vt.env_pos / vi.env_release);
                             } else {
                                 vt.life_count = 0;
                                 vt.env_volume = 0;
@@ -176,7 +191,7 @@ impl PtPlug {
                             vt.env_pos += 1;
                         }
                     }
-    
+
                     update_env(vi, vt, tone.on);
                 }
             }
@@ -188,7 +203,10 @@ impl PtPlug {
 
                     for v in 0..self.pxtn_woice._voice_num as usize {
                         let vi = &mut {
-                            std::slice::from_raw_parts_mut(self.pxtn_woice._voinsts, pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _)
+                            std::slice::from_raw_parts_mut(
+                                self.pxtn_woice._voinsts,
+                                pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _,
+                            )
                         }[v];
                         let vt = &mut tone.voice_tones[v];
 
@@ -197,7 +215,7 @@ impl PtPlug {
                         if vt.life_count > 0 {
                             let pos = vt.smp_pos as i32 * 4 + ch as i32 * 2;
                             work += *vi.p_smp_w.offset(pos as _).cast::<i16>() as i32;
-                            
+
                             if dst_ch == 1 {
                                 work += *vi.p_smp_w.offset((pos + 2) as _).cast::<i16>() as i32;
                                 work /= 2;
@@ -212,7 +230,10 @@ impl PtPlug {
                                 work = (min_ct - smooth) / smooth;
                             }
 
-                            vt.smp_pos += vi.smp_body_w as f64 * tone.note_freq as f64 / self.sample_rate as f64 * vt.offset_freq as f64 / 4.0;
+                            vt.smp_pos += vi.smp_body_w as f64 * tone.note_freq as f64
+                                / self.sample_rate as f64
+                                * vt.offset_freq as f64
+                                / 4.0;
 
                             if loop_ {
                                 if vt.smp_pos >= vi.smp_body_w as _ {
@@ -243,7 +264,7 @@ impl PtPlug {
 
                         pan_buf += work;
                     }
-                    
+
                     tone.time_pan_buf[ch][self.time_pan_index] = pan_buf;
                 }
             }
@@ -255,16 +276,19 @@ impl PtPlug {
                 let mut work: i32 = 0;
 
                 for tone in &self.tones {
-                    let index = (self.time_pan_index as i32 - self.pxtn_time_pan[ch]) & (pxtone_sys::pxtnBUFSIZE_TIMEPAN - 1) as i32;
+                    let index = (self.time_pan_index as i32 - self.pxtn_time_pan[ch])
+                        & (pxtone_sys::pxtnBUFSIZE_TIMEPAN - 1) as i32;
                     work += tone.time_pan_buf[ch][index as usize];
                 }
 
                 out[ch] += (work as f32 / 0x7fff as f32).clamp(-1.0, 1.0);
             }
 
-            self.time_pan_index = (self.time_pan_index + 1) & (pxtone_sys::pxtnBUFSIZE_TIMEPAN as usize - 1);
+            self.time_pan_index =
+                (self.time_pan_index + 1) & (pxtone_sys::pxtnBUFSIZE_TIMEPAN as usize - 1);
 
-            self.tones.retain(|t| t.voice_tones.iter().any(|vt| vt.life_count > 0));
+            self.tones
+                .retain(|t| t.voice_tones.iter().any(|vt| vt.life_count > 0));
 
             out
         }
@@ -308,16 +332,26 @@ impl Plugin for PtPlug {
         _context: &mut impl InitContext<Self>,
     ) -> bool {
         self.sample_rate = buffer_config.sample_rate;
-            
+
         unsafe {
-            if !self.pxtn_serv.set_destination_quality(2, self.sample_rate as i32) {
+            if !self
+                .pxtn_serv
+                .set_destination_quality(2, self.sample_rate as i32)
+            {
                 panic!("serv.set_destination_quality() failed");
             }
-    
-            match self.pxtn_woice.Tone_Ready(std::ptr::null(), self.sample_rate as i32) {
+
+            match self
+                .pxtn_woice
+                .Tone_Ready(std::ptr::null(), self.sample_rate as i32)
+            {
                 0 => {
                     if let Some(logger) = &self.logger {
-                        logger.log(&RecordBuilder::new().args(format_args!("woice.Tone_Ready OK")).build());
+                        logger.log(
+                            &RecordBuilder::new()
+                                .args(format_args!("woice.Tone_Ready OK"))
+                                .build(),
+                        );
                     }
                 },
                 n => panic!(
@@ -325,9 +359,17 @@ impl Plugin for PtPlug {
                     CStr::from_ptr(pxtnError_get_string(n)).to_str().unwrap()
                 ),
             }
-    
+
             if let Some(logger) = &self.logger {
-                logger.log(&RecordBuilder::new().args(format_args!("sps {} smp_body_w = {}", self.sample_rate, (*self.pxtn_woice._voinsts).smp_body_w)).build());
+                logger.log(
+                    &RecordBuilder::new()
+                        .args(format_args!(
+                            "sps {} smp_body_w = {}",
+                            self.sample_rate,
+                            (*self.pxtn_woice._voinsts).smp_body_w
+                        ))
+                        .build(),
+                );
             }
         }
 
@@ -368,25 +410,24 @@ impl Plugin for PtPlug {
                                 note_id: note,
                                 note_freq: util::midi_note_to_freq(note),
                                 velocity: (velocity * 127.0) as _,
-                                voice_tones: [
-                                    pxtnVOICETONE {
-                                        smp_pos: 0.0,
-                                        offset_freq: 1.0,
-                                        env_volume: 128,
-                                        life_count: 1,
-                                        on_count: 0,
-                                        smp_count: 0,
-                                        env_start: 0,
-                                        env_pos: 0,
-                                        env_release_clock: 0,
-                                        smooth_volume: 0,
-                                    }; pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _
-                                ],
-                                time_pan_buf: [[0; pxtone_sys::pxtnBUFSIZE_TIMEPAN as _]; pxtone_sys::pxtnMAX_CHANNEL as _],
+                                voice_tones: [pxtnVOICETONE {
+                                    smp_pos: 0.0,
+                                    offset_freq: 1.0,
+                                    env_volume: 128,
+                                    life_count: 1,
+                                    on_count: 0,
+                                    smp_count: 0,
+                                    env_start: 0,
+                                    env_pos: 0,
+                                    env_release_clock: 0,
+                                    smooth_volume: 0,
+                                };
+                                    pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _],
+                                time_pan_buf: [[0; pxtone_sys::pxtnBUFSIZE_TIMEPAN as _];
+                                    pxtone_sys::pxtnMAX_CHANNEL as _],
                             };
 
                             unsafe {
-
                                 self.pxtn_vol_pan[0] = 64;
                                 self.pxtn_vol_pan[1] = 64;
                                 if dst_ch == 2 {
@@ -409,19 +450,24 @@ impl Plugin for PtPlug {
                                         if self.pxtn_time_pan[0] >= 64 {
                                             self.pxtn_time_pan[0] = 63;
                                         }
-                                        self.pxtn_time_pan[0] = self.pxtn_time_pan[0] * 44100 / self.sample_rate as i32;
+                                        self.pxtn_time_pan[0] =
+                                            self.pxtn_time_pan[0] * 44100 / self.sample_rate as i32;
                                     } else {
                                         self.pxtn_time_pan[1] = 64 - time_pan;
                                         if self.pxtn_time_pan[1] >= 64 {
                                             self.pxtn_time_pan[1] = 63;
                                         }
-                                        self.pxtn_time_pan[1] = self.pxtn_time_pan[1] * 44100 / self.sample_rate as i32;
+                                        self.pxtn_time_pan[1] =
+                                            self.pxtn_time_pan[1] * 44100 / self.sample_rate as i32;
                                     }
                                 }
 
                                 for v in 0..self.pxtn_woice._voice_num as usize {
                                     let vi = &mut {
-                                        std::slice::from_raw_parts_mut(self.pxtn_woice._voinsts, pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _)
+                                        std::slice::from_raw_parts_mut(
+                                            self.pxtn_woice._voinsts,
+                                            pxtone_sys::pxtnMAX_UNITCONTROLVOICE as _,
+                                        )
                                     }[v];
                                     let vt = &mut tone.voice_tones[v];
                                     let vu = &*self.pxtn_woice.get_voice(v as _);
@@ -430,11 +476,13 @@ impl Plugin for PtPlug {
                                     vt.smp_pos = 0.0;
                                     vt.smp_count = 0;
                                     vt.env_pos = 0;
-    
+
                                     if vu.voice_flags & pxtone_sys::PTV_VOICEFLAG_BEATFIT != 0 {
                                         // ?
                                     } else {
-                                        vt.offset_freq = self.pxtn_freq.Get(pxtone_sys::EVENTDEFAULT_BASICKEY as i32 - vu.basic_key) * vu.tuning;
+                                        vt.offset_freq = self.pxtn_freq.Get(
+                                            pxtone_sys::EVENTDEFAULT_BASICKEY as i32 - vu.basic_key,
+                                        ) * vu.tuning;
                                     }
 
                                     if vi.env_size != 0 {
@@ -448,11 +496,13 @@ impl Plugin for PtPlug {
                             }
 
                             self.tones.push(tone);
-                        }
+                        },
                         NoteEvent::NoteOff { note, .. } => {
                             self.master_gain.set_target(self.sample_rate, 0.0);
 
-                            if let Some(tone) = self.tones.iter_mut().find(|t| t.on && t.note_id == note) {
+                            if let Some(tone) =
+                                self.tones.iter_mut().find(|t| t.on && t.note_id == note)
+                            {
                                 tone.on = false;
                                 for v in 0..self.pxtn_woice._voice_num as usize {
                                     let vt = &mut tone.voice_tones[v];
@@ -460,13 +510,13 @@ impl Plugin for PtPlug {
                                     vt.env_pos = 0;
                                 }
                             }
-                        }
+                        },
                         NoteEvent::PolyPressure { note: _, pressure, .. } => {
                             self.master_gain.set_target(self.sample_rate, pressure);
-                        }
+                        },
                         NoteEvent::Choke { note, .. } => {
                             self.tones.retain(|t| t.note_id != note);
-                        }
+                        },
                         _ => (),
                     }
 
@@ -487,10 +537,10 @@ impl Plugin for PtPlug {
 
 impl ClapPlugin for PtPlug {
     const CLAP_ID: &'static str = "me.pieking1215.pxtoneplug";
-    const CLAP_DESCRIPTION: Option<&'static str> =
-        Some("Plays pxtone woices");
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("Plays pxtone woices");
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
-    const CLAP_SUPPORT_URL: Option<&'static str> = Some("https://github.com/PieKing1215/pxtone-plug/issues");
+    const CLAP_SUPPORT_URL: Option<&'static str> =
+        Some("https://github.com/PieKing1215/pxtone-plug/issues");
     const CLAP_FEATURES: &'static [ClapFeature] = &[
         ClapFeature::Instrument,
         ClapFeature::Synthesizer,
@@ -501,10 +551,8 @@ impl ClapPlugin for PtPlug {
 
 impl Vst3Plugin for PtPlug {
     const VST3_CLASS_ID: [u8; 16] = *b"pxtonePlug______";
-    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] = &[
-        Vst3SubCategory::Instrument,
-        Vst3SubCategory::Synth,
-    ];
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
+        &[Vst3SubCategory::Instrument, Vst3SubCategory::Synth];
 }
 
 nih_export_clap!(PtPlug);
